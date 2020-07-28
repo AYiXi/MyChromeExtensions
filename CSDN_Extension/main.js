2d1bb8fdc08897567da6b142c181b4adee5a0e93
Ayixi <libw@csdn.net>
2020-07-28 10:09:04 +0800

    add video speed controller

--- CSDN_Extension/main.js
+++ CSDN_Extension/main.js
@@ -1,9 +1,19 @@
+function click_more() {
+    btn = document.getElementById('btn-readmore-zk')
+    // console.log(btn)
+    if (btn != null) {
+        btn.click()
+    }// } else {
+    //     console.log(111)
+    // }
+}
+
 function change_url() {
     // $('a').each((_, a) => {
     //     var href = a.href
     //     a.href = href.replace('blog.csdn.net', 'test-blog.csdn.net')
     // })
-    
+
     var urls = document.getElementsByTagName('a')
     for (var v in urls) {
         var href = urls[v].href
@@ -13,6 +23,7 @@ function change_url() {
     }
 }
 
-window.onload = function() {
+window.onload = function () {
+    click_more()
     change_url()
 }
